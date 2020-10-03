b073fb858e18702418d097f42097fab8be3a5836
C-90 Abdulaziz Abdullah <70667610+3azooz01@users.noreply.github.com>
2020-10-03 17:39:48 +0300

    a

---
+++ Abdulaziz ABdullah/PokemonTrainerApp/app/src/main/java/com/example/pokemontrainerapp/pokemons.java
@@ -0,0 +1,57 @@
+package com.example.pokemontrainerapp;
+
+public class pokemons {
+    private String name;
+    private int image;
+    private int attack;
+    private int defense;
+    private int total;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public pokemons(String name, int image, int attack, int defense, int total) {
+        this.name = name;
+        this.image = image;
+        this.attack = attack;
+        this.defense = defense;
+        this.total = total;
+    }
+}
